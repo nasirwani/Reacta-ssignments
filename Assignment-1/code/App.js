db11983f9c5b0e3e0bef0583856c0348dd17e312
Nasir <waninasie@gmail.com>
2022-03-29 23:46:51 +0530

    assignment2 completed

--- Assignment-1/code/App.js
+++ Assignment-1/code/App.js
@@ -1,33 +1,25 @@
 /** @format */
-// import React from "react";
-// function App() {
-//   return (
-//     <>
-//       <div className="container">
-//         <div className="first-row">
-//           <div className="row1"></div>
-//           <div className="row22"></div>
-//           <div className="row2"></div>
-//           <div className="row12"></div>
-//           <div className="row21"></div>
-//         </div>
-//         <div className="second-row">
-//           <div className="row3"></div>
-//           <div className="row4"></div>
-//           <div className="row5"></div>
-//           <div className="row6"></div>
-//           <div className="row7"></div>
-//         </div>
-//         <div className="third-row">
-//           <div className="row8"></div>
-//           <div className="row81"></div>
-//           <div className="row82"></div>
-//           <div className="row83"></div>
-//           <div className="row84"></div>
-//           <div className="row85"></div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-// export default App;
+import React from "react";
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="first-row">
+          <div className="row1"></div>
+          <div className="row2"></div>
+        </div>
+        <div className="second-row">
+          <div className="row3"></div>
+          <div className="row4"></div>
+        </div>
+        <div className="third-row">
+          <div className="row5"></div>
+          <div className="row6"></div>
+          <div className="row7"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
