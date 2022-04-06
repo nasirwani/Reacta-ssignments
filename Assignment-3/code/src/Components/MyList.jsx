5d09a61b6f4d39b068044fe30350ce555ba57a26
Nasir <waninasie@gmail.com>
2022-04-06 12:34:41 +0530

    assignment3 completed

---
+++ Assignment-3/code/src/Components/MyList.jsx
@@ -0,0 +1,19 @@
+/** @format */
+
+const arr = [
+  {
+    id: 1,
+    name: "Alan Ford",
+    employeeId: 35973,
+    appointment: 397103001,
+    email: "abc@gmail.com",
+    phone: 100000000391,
+    status: "in progress",
+    door: "Mark",
+    time: "10:30",
+    prodlist: "AlanFord Bobseen",
+    des: "lorem ipsum",
+    profileImg: "https://www.w3schools.com/howto/img_avatar.png",
+  },
+];
+export default arr;
